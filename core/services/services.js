e20337373bd4d46d7b2116614dc6ceeb1992d70a
SrineshNisala <srineshnisala@gmail.com>
2016-07-08 23:44:04 +0530

    edied

--- core/services/services.js
+++ core/services/services.js
@@ -2,16 +2,31 @@ const fs = require('fs');
 const path = require('path');
 
 /*****************Service Interface*****************/
-var Services = function (){}
+var Services = function () { }
+
+
+/*****************Common Properties*****************/
+
+
+
+/*****************Common Functions*****************/
+
+
 
 /*****************User Exceptions*****************/
-Services.prototype.userException  = function (name, message, htmlMessage) {
+Services.prototype.userException = function (name, message, htmlMessage) {
   this.name = name;
   this.message = message;
   this.htmlMessage = htmlMessage;
   Error.captureStackTrace(this);
 }
-Services.prototype.create = new Function;
+
+
+
+
+
+
+
 
 
 module.exports = Services;
---
+++ test/sample.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('a basic test', function () {
+  it('should be fine', function () {
+    expect(true).to.be.true;
+  })
+})
