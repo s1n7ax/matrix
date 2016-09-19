8f329f9e4a726b92bce7f55e0b4cd45275f93581
SrineshNisala <srineshnisala@gmail.com>
2016-09-19 23:26:38 +0530

    couchdb database is added. deleted unwanted files

--- common/user_exception.js
+++
@@ -1,7 +0,0 @@
-//let UserException = function  (name, message, htmlMessage) {
-//	this.name = name;
-//	this.message = message;
-//	this.htmlMessage = htmlMessage;
-//}
-
-module.exports = UserException;
