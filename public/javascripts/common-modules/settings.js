81405e861c2b1d02ea3f04346c0cb4d0ee1b9527
SrineshNisala <SrineshNisala@gmail.com>
2017-05-30 19:55:17 +0530

    added mobile commands

--- public/javascripts/common-modules/settings.js
+++ public/javascripts/common-modules/settings.js
@@ -1,6 +1,6 @@
 APPLICATION_NAME = '';
 
-APPLICATION_COMMAND_LIST = [
+VTAF_COMMANDS = [
     "Comment", "KeyPress", "GetObjectCount", "CheckDBResults", "SetDBResult", "CreateDBConnection", "Screenshot", "Fail", "Loop",
     "Else", "CheckChartContent", "If", "CheckPattern", "NavigateToURL", "Type", "DoubleClick", "Call", "SwitchUser", "ClickAt",
     "Click", "EditVariable", "CheckElementPresent", "GoBack", "Select", "Retrieve", "EndLoop", "MouseMoveAndClick", "SetVarProperty",
@@ -8,6 +8,12 @@ APPLICATION_COMMAND_LIST = [
     "CheckTable", "CreateUser", "SetVariable", "Store", "StartComment", "Pause", "CheckObjectProperty", "MouseOver", "Open", "EndComment",
     "CheckDocument", "HandleImagePopup", "CheckImagePresent", "RightClick", "Break"
 ];
+
+SEETEST_COMMANDS = ["Launch", "Tap", "Swipe", "Rotate", "PerformUserActions", "DragToElement", "CheckPattern", "ElementSwipe", "ElementDrag" ,"Landscape", 
+    "Portrait", "Home", "Enter", "Wake", "Unlock", "Browserback", "Menu", "RecentApps", "Power", "CloseKeyboard", "Paste", "Backspace", "Back", "LongTap"];
+
+
+APPLICATION_COMMAND_LIST = VTAF_COMMANDS.concat(SEETEST_COMMANDS);
 
 STARTS_WITH_COMMAND_REGEX = getStartsWithCommandRegex();
 
